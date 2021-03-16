74169b4653111051ef498be85826edc0a5233e10
Aji Manarul Aziz <ajimanarul@gmail.com>
2021-03-16 11:58:30 +0700

    tuple

---
+++ I0320119_Exercise3.9.py
@@ -0,0 +1,10 @@
+tup1 = (12, 34.56)
+tup2 = ('abc', 'xyz')
+
+# Aksi seperti di bawah ini tidak bisa dilakukan pada tuple Python
+# Karena memang nilai pada tuple python tidak bisa diubah
+# tup1[0] = 100
+
+# Jadi, buatlah tuple baru sebagai berikut
+tup3 = tup1 + tup2
+print(tup3)
